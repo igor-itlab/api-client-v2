a9d2266a44d58fa017cf8492cf90521ecf490e5e
Egor-itlab <yegor.krasnov.ua@itlab-studio.com>
2021-07-05 12:54:16 +0300

    add: dependency

--- src/ApiClientBundle.php
+++ src/ApiClientBundle.php
@@ -4,9 +4,18 @@
 namespace ApiClient;
 
 
+use ApiClient\DependencyInjection\ApiClientExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApiClientBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if ((null === $this->extension)) {
+            $this->extension = new ApiClientExtension();
+        }
+
+        return $this->extension;
+    }
 
 }
