27ce5585ef6647a568e85f21fdb874614bcfb2e0
rskrydge@itlab-studio.com <rskrydge@itlab-studio.com>
2021-07-19 10:23:01 +0300

    update api client extension

--- src/DependencyInjection/ApiClientExtension.php
+++ src/DependencyInjection/ApiClientExtension.php
@@ -8,7 +8,6 @@ use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -25,7 +24,7 @@ class ApiClientExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load("services.xml");
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config/'));
+        $loader->load('services.xml');
     }
 }
