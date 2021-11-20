317ae434ba3fbbee3c5615c023b8ed904344ba19
rskrydge@itlab-studio.com <rskrydge@itlab-studio.com>
2021-11-20 16:03:06 +0200

    change domain

--- src/Api/ControlPanel/ControlPanelRequest.php
+++ src/Api/ControlPanel/ControlPanelRequest.php
@@ -36,7 +36,7 @@ class ControlPanelRequest extends Request
     {
 
         return parent::createRequestBuilder()
-            ->setDomain("https://cpv.crpt.trading")
+            ->setDomain("https://cp.crpt.trading")
             ->addHeader(
                 "Accept",
                 "application/json"
